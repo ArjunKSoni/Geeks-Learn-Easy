959d6140fe4b7099eb9bd2773a497691bced0fd7
Arjun Soni <94898141+ArjunKSoni@users.noreply.github.com>
2023-05-22 00:59:06 +0530

    .

--- src/components/body.jsx
+++ src/components/body.jsx
@@ -2,8 +2,12 @@ import React from 'react'
 
 export default function Body() {
     return (
-        <div className=' p-3 text-white body overflow-scroll flex-1'>
-            <div className='text-3xl font-bold p-2'> Heading</div>
+        <div className=' p-3  text-white body overflow-scroll flex flex-1'>
+            <div className='text-3xl pl-3 font-bold p-2 break-words w-3/4'> Heading </div>
+
+            <div className='flex-1'>
+                adds
+            </div>
         </div>
     )
 }
