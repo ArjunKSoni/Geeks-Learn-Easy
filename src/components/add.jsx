8689181f3829b55423a7a4d666de0225f511ec2f
Arjun Soni <94898141+ArjunKSoni@users.noreply.github.com>
2023-05-22 14:43:30 +0530

    content ready

---
+++ src/components/add.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Add() {
+    return (
+        <div className='flex-1 pl-4'>
+            Adds
+        </div>
+    )
+}
--- src/components/body.jsx
+++ src/components/body.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { ImRadioChecked2 } from "react-icons/im";
+import Add from './add';
 
 export default function Body() {
     return (
@@ -9,7 +10,34 @@ export default function Body() {
                 <div className='text-3xl font-bold pb-3 border-slate-500 border-b-4 break-words'> Heading </div>
 
                 {/* paragraph  */}
-                <div className='mt-3'>
+                <div className='mt-2'>
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Atque nesciunt id adipisci iste, modi sunt facilis! Explicabo, ducimus molestias atque eveniet numquam modi, perspiciatis porro beatae quasi ut enim. Temporibus?
+                </div>
+
+                {/* list  */}
+                <ul className='font-bold mt-3'> List heading name:
+                    <li className='ml-4 font-normal flex items-center'><ImRadioChecked2 className='text-xs mt-1 mr-2' /> value 1</li>
+                    {/* List with link */}<li className='ml-4 font-normal flex items-center'><ImRadioChecked2 className='text-xs mt-1 mr-2' />Click me to open<a className='text-green-700 font-semibold underline mx-1' href="https://www.google.com/">google</a>.</li>
+                    <li className='ml-4 font-normal flex items-center'><ImRadioChecked2 className='text-xs mt-1 mr-2' /> value 3</li>
+                </ul>
+
+                {/* sub heading  */}
+                <div className='text-2xl mt-3 font-semibold justify-center items-center flex break-words'>
+                    subheading
+                </div>
+
+                {/* paragraph  */}
+                <div className='mt-2'>
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Atque nesciunt id adipisci iste, modi sunt facilis! Explicabo, ducimus molestias atque eveniet numquam modi, perspiciatis porro beatae quasi ut enim. Temporibus?
+                </div>
+
+                {/* image  */}
+                <div className='flex items-center justify-center'>
+                    <div><img src="logo192.png" alt="" width={"150px"} height={"150px"} /><h4 className=' text-center text-xs'>Name of image</h4></div>
+                </div>
+
+                {/* paragraph  */}
+                <div className='mt-2'>
                     Lorem ipsum dolor sit, amet consectetur adipisicing elit. Atque nesciunt id adipisci iste, modi sunt facilis! Explicabo, ducimus molestias atque eveniet numquam modi, perspiciatis porro beatae quasi ut enim. Temporibus?
                 </div>
 
@@ -20,20 +48,19 @@ export default function Body() {
                     <li className='ml-4 font-normal flex items-center'><ImRadioChecked2 className='text-xs mt-1 mr-2' /> value 3</li>
                 </ul>
 
-                {/* sub heading  */}
-                <div className='text-2xl mt-3 font-semibold pb-3 break-words'>
-                    subheading
-                </div>
-
             </div>
-
-            <div className='flex-1 pl-4'>
-                Adds
-            </div>
+            <Add />
         </div>
     )
 }
 
 // for some bold words in between =>   <div className='font-bold'>your word</div>
+
 // words with greater text size =>     <div className='text-xl'>your word</div>
+
 // for links in between =>             <a className='text-green-700 font-semibold underline mx-1' href="https://www.google.com/">google</a>
+
+// {/* image  */}
+// <div className='flex items-center justify-center'>
+// <div><img src="logo192.png" alt="" width={"150px"} height={"150px"} /><h4 className=' text-center text-xs'>Name of image</h4></div>
+// </div>
