f88302b3ce9a06054e07e45165ae27f23244866b
Arjun Soni <94898141+ArjunKSoni@users.noreply.github.com>
2023-05-26 10:52:15 +0530

    news

--- src/components/news.jsx
+++ src/components/news.jsx
@@ -2,8 +2,44 @@ import React from 'react'
 
 export default function News() {
     return (
-        <div className='home min-h-screen text-white'>
-            News
+        <div className='home min-h-screen flex flex-col items-center justify-start text-white'>
+            <h1 id="title">Latest News</h1>
+            <div className="news-section">
+                <div className="img-left">
+                    <img src="https://picsum.photos/300/300" alt="news-image" />
+                </div>
+                <div className="news-content">
+                    <h2>Lorem ipsum dolor sit amet.</h2>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod cupiditate natus pariatur
+                        exercitationem. Voluptatibus adipisci aspernatur expedita minima debitis qui ipsum dicta excepturi
+                        voluptates magni!</p>
+                    <a href="#">Read More</a>
+                </div>
+            </div>
+            <div className="news-section">
+                <div className="img-left">
+                    <img src="https://picsum.photos/300/300" alt="news-image" />
+                </div>
+                <div className="news-content">
+                    <h2>Lorem ipsum dolor sit amet.</h2>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod cupiditate natus pariatur
+                        exercitationem. Voluptatibus adipisci aspernatur expedita minima debitis qui ipsum dicta excepturi
+                        voluptates magni!</p>
+                    <a href="#">Read More</a>
+                </div>
+            </div>
+            <div className="news-section">
+                <div className="img-left">
+                    <img src="https://picsum.photos/300/300" alt="news-image" />
+                </div>
+                <div className="news-content">
+                    <h2>Lorem ipsum dolor sit amet.</h2>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod cupiditate natus pariatur
+                        exercitationem. Voluptatibus adipisci aspernatur expedita minima debitis qui ipsum dicta excepturi
+                        voluptates magni!</p>
+                    <a href="#">Read More</a>
+                </div>
+            </div>
         </div>
     )
 }
