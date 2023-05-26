85d11fb62f14fbba5495ab8a0fb0d14189364388
Arjun Soni <94898141+ArjunKSoni@users.noreply.github.com>
2023-05-26 11:05:34 +0530

    bug fixed

--- src/components/news.jsx
+++ src/components/news.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function News() {
     return (
         <div className='home min-h-screen flex flex-col items-center justify-start text-white'>
-            <h1 id="title">Latest News</h1>
+            <h1 id="news-title">Latest News</h1>
             <div className="news-section">
                 <div className="img-left">
                     <img src="https://picsum.photos/300/300" alt="news-image" />
