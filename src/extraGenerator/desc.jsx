470ad7926ec09505b355c5140538fa82555eed49
Arjun Soni <94898141+ArjunKSoni@users.noreply.github.com>
2023-06-08 14:36:56 +0530

    .

--- src/extraGenerator/desc.jsx
+++ src/extraGenerator/desc.jsx
@@ -1,20 +1,19 @@
 const desc = [
     {
         "DBMS": "Database Management System",
-        "Machine Learning and Optimization": "building methods that let machines learn",
+        "Machine Learning": "building methods that let machines learn",
         "Operating System": "System software that manages computer hardware and software",
-        "Network and Web Security": "Protects your data from threats",
+        "Network-Web Security": "Protects your data from threats",
         "Data Structures": "Data organization format for efficient access to data.",
         "CNP": "Computer Network and Protocols",
         "Cloud Computing": "On-demand availability of computer system resources",
-        "Design and Analysis of Algorithm": "Analyzing the solution before coding",
+        "DAA": "Design and Analysis of Algorithm",
         "Computer Architecture": "Structure of a computer system",
         "Engineering Chemistry": "Deals with the study of chemical plants ",
-        "Intro to AI": "Simulation of human intelligence processes by machines",
-        "Intro to Probability and Statistics": "Estimate of how likely an event might take place",
+        "Introduction to AI": "Simulation of human intelligence processes by machines",
+        "Probability and Statistics": "IPS- Estimate of how likely an event might take place",
         "Soft Computing": "Set of algorithms, including neural networks",
         "Software Engineering": "Engineering-based approach to software development",
         "Technical Communication": "Communication of technical subject matter such as engineering"
     }
 ]
-export default desc
--- src/extraGenerator/homeSubject.jsx
+++ src/extraGenerator/homeSubject.jsx
@@ -16,7 +16,7 @@ export default function HomeSubject(props) {
     }
     return (
         <Link style={Theme[theme].boxDown} onClick={clicked} className='box2 m-4'>
-            <div style={{ backgroundColor: colour, height: "40%" }} className="title2 w-full text-2xl font-bold flex items-center justify-center">{name}</div>
+            <div style={{ backgroundColor: colour, height: "40%" }} className="title2 w-full text-2xl text-center p-1 font-bold flex items-center justify-center">{name}</div>
             <div style={{ height: "60%" }} className="sub2 flex items-center font-bold text-center justify-center">{desc}</div>
         </Link>
     )
