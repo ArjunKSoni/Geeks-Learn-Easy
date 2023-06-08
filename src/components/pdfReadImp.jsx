151f283c704557100a5de4bd07266a8cdef604e8
Arjun Soni <94898141+ArjunKSoni@users.noreply.github.com>
2023-06-08 13:31:06 +0530

    .

--- src/components/pdfReadImp.jsx
+++ src/components/pdfReadImp.jsx
@@ -5,7 +5,7 @@ import Authcontext from '../context/authContext'
 import { Link, useLocation } from 'react-router-dom'
 import Subcontext from '../context/subContext'
 import { useEffect } from 'react'
-// import { AiOutlineArrowLeft } from 'react-icons/ai';
+import { BsArrowBarRight } from 'react-icons/bs';
 
 export default function PdfReadImp() {
     const location = useLocation()
@@ -18,10 +18,10 @@ export default function PdfReadImp() {
     }, [])
     return (
         <div className="flex h-screen">
-            {/* <Link to={"/subject"}><div style={Theme[theme].bgtext} className={`absolute transition-all text-3xl top-4 left-4 rounded-full p-3`}><AiOutlineArrowLeft /></div></Link> */}
+            <div style={Theme[theme].bgtext} className={`absolute transition-all text-3xl top-4 left-4 rounded-full p-3`}><BsArrowBarRight /></div>
             <div style={Theme[theme].bodydesc2} className="contentSidebar p-3 min-h-full sideDec no-scrollbar border-r-4 overflow-x-auto">
                 {item.map((e, i) => {
-                    return (<ul style={Theme[theme].bodydesc2} className='navButton'>
+                    return (<ul key={i} style={Theme[theme].bodydesc2} className='navButton'>
                         <input readOnly={true} onClick={(e) => { e.currentTarget.focus(); setreadpdf(item[i][`imp${i + 1}`]) }} className={` ml-3 p-1 px-2 text-gray-500 rounded-xl hover:text-white hover:${Theme[theme].newdescHover}`} value={`imp${i + 1}`} />
                     </ul>
                     )
--- src/components/pdfread.jsx
+++ src/components/pdfread.jsx
@@ -5,7 +5,7 @@ import Authcontext from '../context/authContext'
 import { Link, useLocation } from 'react-router-dom'
 import Subcontext from '../context/subContext'
 import { useEffect } from 'react'
-// import { AiOutlineArrowLeft } from 'react-icons/ai';
+import { BsArrowBarRight } from 'react-icons/bs';
 
 export default function Pdfread() {
     const location = useLocation()
@@ -17,16 +17,17 @@ export default function Pdfread() {
     }, [])
     return (
         <div className="flex h-screen">
-            {/* <Link to={"/subject"}><div style={Theme[theme].bgtext} className={`absolute transition-all text-3xl top-4 left-4 rounded-full p-3`}><AiOutlineArrowLeft /></div></Link> */}
-            <div style={Theme[theme].bodydesc2} className="contentSidebar p-3 min-h-full sideDec no-scrollbar border-r-4 overflow-x-auto">
+            {/* <div className={`absolute bg-black readpdf text-white transition-all text-3xl top-4 left-1 rounded-full p-3`}><BsArrowBarRight /></div> */}
+            <div><div style={Theme[theme].bodydesc2} className="contentSidebar p-3 min-h-full sideDec no-scrollbar border-r-4 overflow-x-auto">
                 {item.map((e, i) => {
-                    return (<ul style={Theme[theme].bodydesc2} className='navButton'> <h1 className={`p-1 px-2 text-xl text-white rounded-xl font-bold hover:text-white  hover:${Theme[theme].newdescHover}`}>{`Unit ${i + 1}`}</h1>
+                    return (<ul key={i} style={Theme[theme].bodydesc2} className='navButton'> <h1 className={`p-1 px-2 text-xl text-white rounded-xl font-bold hover:text-white  hover:${Theme[theme].newdescHover}`}>{`Unit ${i + 1}`}</h1>
                         {e[`unit${i + 1}`].map((q, j) => {
-                            return <input readOnly={true} onClick={(e) => { e.currentTarget.focus(); setreadpdf(q[`pdf${j + 1}`]) }} className={` ml-3 p-1 px-2 text-gray-500 rounded-xl hover:text-white hover:${Theme[theme].newdescHover}`} value={`Pdf${j + 1}`} />
+                            return <input key={j} readOnly={true} onClick={(e) => { e.currentTarget.focus(); setreadpdf(q[`pdf${j + 1}`]) }} className={` ml-3 p-1 px-2 text-gray-500 rounded-xl hover:text-white hover:${Theme[theme].newdescHover}`} value={`Pdf${j + 1}`} />
                         })}
                     </ul>
                     )
                 })}
+            </div>
             </div>
             <Pdf />
         </div>
